8f86bc975367794cb8c61b39b230ab5d396813e2
seonghbae <fullbloomh3ck@gmail.com>
2022-05-06 07:07:07 +0900

    HTML, CSS 적용

--- database.js
+++ database.js
@@ -1,130 +1,70 @@
-import { showTodoList } from "./index.js"
+// Dexie로 데이터베이스 생성
+const db = new Dexie("tomato-do");
 
-// 'tomato-do'라는 데이터베이스 생성
-// 안에 여러 Object Store(객체 저장소) 존재 가능
-const onRequest = indexedDB.open('tomato-do', 1)
+// 객체 저장소를 초기화(생성)하고 초기 데이터 삽입
+const initDatabase = async () => {
+    // 객체 저장소 생성 (스키마 설정)
+    // ++ : 자동으로 증가하는 기본 키
+    db.version(1).stores({
+        todolist: "++id, todo, startTime, endTime, check"
+    });
 
-// 데이터베이스 생성후 모든 요청이 성공적일 경우
-// (onupgradeneeded 후에)
-onRequest.onsuccess = async () => {
-    // 
-    const initialData = await getEntryFromDb("todolist")
-    // 'initialData'는 배열인데, 길이가 0이라면 초기 데이터가 없다는 의미이며, 0이 아니면 있는 경우
-    const isInitialData = initialData.length === 0 ? false : true
+    // Dexie 생성시 new Dexie(databaseName, options?);
+    // options 중 데이터베이스를 자동으로 열어주는 'autoOpen' 존재
+    // 'autoOpen'의 default 값이 true이므로 따로 open하지 않음
 
-    if (!isInitialData) {
-        // 초기 데이터 삽입
-        addEntryToDb('todolist', '복습하기')
-        addEntryToDb('todolist', '넷플릭스 보기')
+    const dataCount = await db.todolist.count();
+
+    // 객체 저장소의 데이터가 존재하는 경우
+    if(dataCount > 0) {
+        return Promise.resolve();
     }
 
-    showTodoList();
-}
-
-// 1. 기존의 저장된 데이터베이스보다 큰 버전 번호의 데이터베이스가 로드 될 때
-// 2. 데이터베이스를 처음 만들어서 첫 객체 저장소를 생성할 때
-onRequest.onupgradeneeded = () => {
-    const database = onRequest.result
-    // 'todolist'라는 이름의 겍체 저장소를 생성
-    database.createObjectStore('todolist', {autoIncrement: true})
-}
-
-// 에러 처리
-onRequest.onerror = () => {
-    alert('Error creating or accessing db')
-}
-
-// 이름이 'storeName'인 객체 저장소에서 값이 'entry'인 객체를 찾아 프로미스로 키를 반환합니다.
-// indexedDB의 데이터 접근이 비동기밖에 안되서 새 Promise를 생성하는 식으로 해야 합니다.
-const getKeyFromDb = (storeName, entry) => {
-
-    const data = new Promise((resolve, reject) => {
-        // 현재 데이터베이스를 가져옵니다.
-        const database = onRequest.result
-        // 트랜잭션 : 데이터베이스에서 객체를 지정
-        // 트랜잭션 이루 객체 저장소에 접근 권한이 주어집니다.
-        // 기본이 'readonly'이기때문에 생략가능
-        const transaction = database.transaction([storeName])
-        // 접근권한 주어졌으므로 원하는 객체 저장소를 불러옵니다.
-        const store = transaction.objectStore(storeName)
-    
-        // 객체 저장소에서 객체를 살펴가며 값이 'entry'인 객체를 탐색
-        // 'openCursor()' 자체가 비동기 통신이라 성공 실패 여부를 따지는 것으로 추정
-        const request = store.openCursor()
-        request.onerror = () => {
-            console.log('error getting data from the store')
-            reject(request.error)
-        }
-    
-        request.onsuccess = () => {
-            let cursor = request.result;
-            // console.log(cursor);
-            if(cursor) {
-                if(cursor.value === entry) {
-                    resolve(cursor.key);
-                } else cursor.continue();
-            }
-        }
-    })
-  
-    return Promise.resolve(data)
+    // 초기 데이터 삽입
+    // 기본적으로 Dexie의 CRUD 함수들을 Promise로 제공
+    return await db.todolist.bulkAdd([
+        { todo: "복습하기", startTime: 0, endTime: 0, check: false },
+        { todo: "넷플릭스 시청", startTime: 0, endTime: 0, check: false }
+    ]);
 }
 
 // 이름이 'storeName'인 객체 저장소에 값이 'entry'인 객체를 저장
-const addEntryToDb = (storeName, entry) => {
-    const database = onRequest.result;
-    // 새로운 객체를 추가할 것이기 때문에 'readwrite' 모드로 접근권한을 요청
-    const transaction = database.transaction([storeName], "readwrite");
-    const store = transaction.objectStore(storeName);
-    // 객체 저장소에 객체는 (키, 객체) 형식으로 저장되며
-    // 키를 설정하지 않은 경우 자동적으로 1부터 붙여집니다.
-    store.add(entry);
+const addEntryToDb = async (storeName, entry) => {
+    return await db[storeName].add(entry);
 }
 
 // 이름이 'storeName'인 객체 저장소에서 데이터를 가져옵니다.
-// id가 존재하는 경우 해당 id의 데이터만 가져옵니다.
-const getEntryFromDb = (storeName, id) => {
-    // 비동기 처리로 데이터를 가져옵니다. 
-    // 데이터를 가져오는 행위는 항상 비동기로 이루어집니다.
-    const data = new Promise((resolve, reject) => {
-        const database = onRequest.result
-        const transaction = database.transaction([storeName])
-        const store = transaction.objectStore(storeName)
-        
-        const request = id ? store.get(id) : store.getAll()
-        request.onerror = () => {
-            reject(request.error)
-            console.log('error getting data from the store')
-        }
+// condition이 존재하는 경우 해당 condition의 데이터만 가져옵니다.
+// 비동기 처리로 데이터를 가져옵니다. 
+// 데이터를 가져오는 행위는 항상 비동기로 이루어집니다.
+const getEntryFromDb = async (storeName, condition) => {
+    return (condition) ? await db[storeName].get(condition) : await db[storeName].toArray();
+}
 
-        request.onsuccess = () => {
-            resolve(request.result)
-        }
-    })
-  
-    return Promise.resolve(data)
+// 이름이 'storeName'인 객체 저장소의 값이 'entry'인 객체의 'changes'부분을 갱신
+const updateEntryToDb = async (storeName, entry, changes) => {
+    const item = await getEntryFromDb(storeName, entry);
+    await db[storeName].update(item.id, changes);
+}
+
+// 이름이 'storeName'인 객체 저장소에서 값이 'entry'인 객체를 체크
+const checkEntryFromDb = async (storeName, entry) => {
+    const item = await getEntryFromDb(storeName, entry);
+    item.check = true;
+    return await db[storeName].update(item.id, item);
 }
 
 // 이름이 'storeName'인 객체 저장소에서 값이 'entry'인 객체를 삭제
 const deleteEntryFromDb = async (storeName, entry) => {
-    const id = await getKeyFromDb(storeName, entry);
-
-    const database = onRequest.result;
-    const transaction = database.transaction([storeName], "readwrite");
-    const store = transaction.objectStore(storeName);
-    
-    const request = store.delete(id);
-    // 'index.js' 의 'deleteTodo' 에서 'showTodoList'을 사용하기 위해 promise로 반환
-    return new Promise((resolve, reject) => {
-        request.onerror = () => {
-            reject(request.error)
-            console.log('error getting data from the store')
-        }
-    
-        request.onsuccess = () => {
-            resolve(request.result)
-        }
-    });
+    const item = await getEntryFromDb(storeName, entry);
+    return await db[storeName].delete(item.id);
 }
 
-export { addEntryToDb, getEntryFromDb, deleteEntryFromDb }
+export { 
+    initDatabase,
+    addEntryToDb,
+    getEntryFromDb,
+    updateEntryToDb,
+    checkEntryFromDb,
+    deleteEntryFromDb
+}
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-import { addEntryToDb, getEntryFromDb, deleteEntryFromDb } from "./database.js"
+import { 
+    initDatabase,
+    addEntryToDb,
+    getEntryFromDb,
+    updateEntryToDb,
+    checkEntryFromDb,
+    deleteEntryFromDb
+} from "./database.js"
 
 const addTodo = (e) => {
     e.preventDefault();
     // 'todolist' 스토리지에 입력으로 들어온 투두리스트 추가
-    addEntryToDb('todolist', todoInput.value);
-    // 추가한 스토리지 출력
-    showTodoList(e);
+    const inputEntry = {
+        todo: todoInput.value,
+        startTime: 0,
+        endTime: 0
+    };
+    addEntryToDb('todolist', inputEntry)
+        .then(() => showTodoList(e));
     // 입력 태그 칸 비우기
     todoInput.value = null;
+}
+
+const completeTodo = (e) => {
+    e.preventDefault();
+}
+
+const updateTodo = (e) => {
+    e.preventDefault();
 }
 
 const deleteTodo = (e) => {
@@ -16,7 +35,10 @@ const deleteTodo = (e) => {
     const todoDelete = e.target.parentNode.parentNode.firstChild;
     // 'deleteEntryFromDb'에 비동기 처리가 들어가 있어서
     // 이렇게 안하면 삭제된 리스트가 반영되지 않습니다.
-    deleteEntryFromDb('todolist', todoDelete.innerText)
+    const deleteEntry = {
+        todo: todoDelete.innerText
+    };
+    deleteEntryFromDb('todolist', deleteEntry)
         .then(() => showTodoList(e));
 }
 
@@ -30,7 +52,7 @@ const showTodoList = async (e) => {
     todoListTag.innerHTML = ""
 
     const todoList = await getEntryFromDb('todolist')
-    todoList.forEach((todo) => {
+    todoList.forEach((entry) => {
         // 백틱으로 할 경우
         // todoListTag.innerHTML += `
         // <li>
@@ -42,30 +64,23 @@ const showTodoList = async (e) => {
         // </li>`
         const todoItemElem = document.createElement("li");
 
-        // const listNameElem = document.createElement("span");
-        // listNameElem.classList.add("list_name");
-        // listNameElem.innerHTML = todo;
-
         const checkboxElem = document.createElement("input");
         checkboxElem.setAttribute("type", "checkbox");
         checkboxElem.setAttribute("name", "checklist");
-        // checkboxElem.addEventListener("click", completeTodo);
+        checkboxElem.addEventListener("click", completeTodo);
         // checkboxElem.innerText = '✔';
-
-        // const checklistElem = document.createElement("label");
-        // checklistElem.setAttribute("for", "chekclist");
 
         const editButtonElem = document.createElement("button");
         editButtonElem.classList.add("list_edit_btn");
         editButtonElem.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
-        //editButtonElem.addEventListener("click", updateTodo);
+        editButtonElem.addEventListener("click", updateTodo);
 
         const deleteButtonElem = document.createElement("button");
         deleteButtonElem.classList.add("list_delete_btn");
         deleteButtonElem.addEventListener("click", deleteTodo);
         deleteButtonElem.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
 
-        todoItemElem.innerHTML += `<span class="list_name">${todo}</span>`;
+        todoItemElem.innerHTML += `<span class="list_name">${entry.todo}</span>`;
         todoItemElem.appendChild(checkboxElem);
         todoItemElem.innerHTML += `<label for="checklist"></label>`;
         todoItemElem.appendChild(editButtonElem);
@@ -82,4 +97,4 @@ const addTodoButton = document.querySelector('#todo_submit_btn')
 
 addTodoButton.addEventListener("click", addTodo)
 
-export { showTodoList };
+initDatabase().then(() => showTodoList());
