993bd649a2b2220fcad131c0c8575667faa80511
jaeyeong815 <jae0ee@naver.com>
2022-05-11 20:13:35 +0900

    최신 timer

--- timer.js
+++ timer.js
@@ -13,6 +13,9 @@ const timerBtnCollect = document.getElementsByClassName(
   "timer_btn_collection"
 )[0];
 
+const startOption25 = document.getElementsByClassName("start_option1")[0];
+const startOption50 = document.getElementsByClassName("start_option2")[0];
+
 //console.log(focusTime.outerText);
 
 let count = 0; //반복 횟수를 지정하기 위한 변수 설정
@@ -22,6 +25,39 @@ let min = ""; //남은 시간 - 분
 let sec = ""; //남은 시간 - 초
 let focusInterval; //stop, reset 타이머 함수에서도 사용하기 위해 함수 밖에 변수 정의
 let restInterval;
+
+let selectOption = "option25"; // 현재 옵션 상태 체크를 위한 전역변수 설정
+// 초기 값 25분이니 option25로 설정 시작
+// 더하여 위 focusTImer, restTimer 값이 없을 시 완전 초기에 저장된 시간이 없어 NaN 오류 발생함
+// 따라서 selectOption과 더불어 focusTimer, restTimer 도 1500, 300으로 초기 설정
+
+function select(e) {
+    resetTimer();
+  // btn 클릭 시 select 함수로 eventlisten하여 해당 버튼 value에 따라 전역변수 selectOption의 값 변경
+  const userPick = e.target.value;
+  if (userPick === startOption25.value) {
+    selectOption = "option25";
+    timerTimeSet(); // 전역변수를 변경했으니 timer를 세팅
+  } else {
+    selectOption = "option50";
+    timerTimeSet();
+  }
+}
+
+//옵션에 따른 timer 초기 셋
+function timerTimeSet() {
+  if (selectOption === "option25") {
+    focusTime.innerHTML = `25:00`;
+    restTime.innerHTML = `05:00`;
+    focusTimer = 1500;
+    restTimer = 300;
+  } else if (selectOption === "option50") {
+    focusTime.innerHTML = `50:00`;
+    restTime.innerHTML = `10:00`;
+    focusTimer = 3000;
+    restTimer = 600;
+  }
+}
 
 function stopTimer() {
   clearInterval(focusInterval); //타이머 중단
@@ -34,10 +70,9 @@ function stopTimer() {
 function resetTimer() {
   clearInterval(focusInterval); //타이머 중단
   //중단 시 설정했던 시간으로 돌아가도록 함(일단 25분으로 설정, 추후 변수 이용하여 입력값을 대입하도록 수정)
-  focusTime.innerHTML = `25:00`;
-  restTime.innerHTML = `05:00`;
-  focusTimer = 1500;
-  restTimer = 300;
+
+  timerTimeSet(); // 처음 시간으로 돌리는 건 timeset과 같으니 그대로 불러옴.
+  // 전역변수 selectOption이 변경되지 않았으므로 바꾼 옵션대로 reset 될 것임.
 
   //버튼 다시 start버튼만 나오도록 구성
   timerStopBtn.style.display = "none";
@@ -54,8 +89,6 @@ function focusStart() {
   //setInterval함수를 이용하여 설정한 시간을 기준으로 타이머 작동(일단 25분으로 고정하여 설정해둠)
   focusInterval = setInterval(() => {
     --focusTimer; //타이머 작동
-    console.log(focusTimer);
-    console.log(restTimer);
 
     min = parseInt(focusTimer / 60); //분 표시
     sec = focusTimer % 60; //초 표시
@@ -95,3 +128,6 @@ function restStart() {
 timerStartBtn.addEventListener("click", focusStart);
 timerStopBtn.addEventListener("click", stopTimer);
 timerResetBtn.addEventListener("click", resetTimer);
+
+startOption25.addEventListener("click", select);
+startOption50.addEventListener("click", select);
